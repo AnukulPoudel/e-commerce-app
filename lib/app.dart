abdd59b7b63678b9d56e73a661fc951897b0b600
Arch_Anukul <anukulpoudel999@gmail.com>
2025-04-23 09:15:25 +0545

    added package smooth_page_indicator and lottie for authentication page

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
       themeMode: ThemeMode.system,
       theme: ThemeData(),
       darkTheme: ThemeData(),
-      home: Placeholder(),
+      home: Scaffold(),
     );
   }
 }
