b140c1d8f4fdaf4504cce0e22ab6b783e3fb8b4b
Arch_Anukul <anukulpoudel999@gmail.com>
2025-04-23 08:13:52 +0545

    splash screen and ndk version changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:e_commerce_app_self/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // TODO: Configure splash screen to remain while app initializes
+  
   runApp(const App());
 }
